1f655d1510a48260457cb722c9cc12853c82073d
Jose Luis <jolui334@gmail.com>
2021-03-16 21:20:36 -0500

    Precipitation Jsonify has been included

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import numpy as np
+import pandas as pd
+
+
+
+# Preparing the data with automap to reflect 
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+# reflect an existing database into a new model
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# Saving references
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 app = Flask(__name__)
 
@@ -15,11 +34,27 @@ def home():
         f"If you want to know the temperature observations from the most recurrent stations go to:<br/>"
         f"/api/v1.0/tobs<br/><br/>"
         f"To know more about max, min and average temperature from a define start date go to:<br/> "
-        f"/api/v1.0/<start><br/><br/>"
+        f"/api/v1.0/startdate<br/><br/>"
         f"To know more about max, min and average temperature from a defined date range go to:<br/> "
-        f"/api/v1.0/<start>/<end><br/>"
+        f"/api/v1.0/startdate/enddate <br/>"
         )
-   
+@app.route("/api/v1.0/precipitation")
+def prcpt():
+
+    # Creating the session
+    session = Session(engine)
+    # # coverting Dates to datetime type
+    # import datetime as dt
+    # recent_mth = session.query(func.max(Measurement.date)).all()
+    # recent_mth
+
+    # Populating the dataflists with data from the past 12 months
+    p_date = {}
+    for row in session.query(Measurement).filter(Measurement.date >= '2016-08-23'):
+        p_date.update({row.date:row.prcp})
+
+    return jsonify(p_date)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
